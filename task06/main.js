6420e50ce4455c5f327659dcbbe9ad09cc773535
VladanStar <vladancupric@gmail.com>
2023-01-04 22:45:05 +0100

    Add

--- task06/main.js
+++ task06/main.js
@@ -4,4 +4,6 @@ const z = true;
 console.log(8 * "2", typeof (8 * "2"))
 console.log("5" + 1, typeof ('5' + 1, 5 - 1, typeof ('5' - 1)));
 console.log(8 * "two", typeof (8 * "two"));
-console.log(8*NaN, typeof NaN);
+console.log(8 * NaN, typeof NaN);
+let recenica = "vladan cupric"
+console.log(recenica.indexOf("nepostojeca"));
