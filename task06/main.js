9f91190191b9a07056991a854c873b22f0e48ea9
VladanStar <vladancupric@gmail.com>
2023-01-05 21:48:58 +0100

    Add

--- task06/main.js
+++ task06/main.js
@@ -113,3 +113,16 @@ let pozicija = cela_recenica.indexOf('recenica');
 console.log('Pozicija niske "recenica" u recenici je:', pozicija);
 pozicija = cela_recenica.indexOf('nepostojeca niska');
 console.log('Pozicija niske "nepostojeca niska" u recenici je:', pozicija);
+
+const neuspesna_konverzija_1 = Number.parseInt('jedan');
+const neuspesna_konverzija_2 = Number.parseInt('dva');
+console.log('Vrednost neuspesne konverzije 1 je ' + neuspesna_konverzija_1);
+console.log('Vrednost neuspesne konverzije 2 je ' + neuspesna_konverzija_2);
+console.log(
+  'Da li dve NaN vrednosti mogu biti jednake?',
+  neuspesna_konverzija_1 == neuspesna_konverzija_2
+);
+console.log(
+  'Provera da li je vrednost NaN:',
+  Number.isNaN(neuspesna_konverzija_1)
+);
