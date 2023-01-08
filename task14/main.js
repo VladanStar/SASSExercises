dcb029fdec3dd27ee5330e81c46a9dc0694a5ef6
VladanStar <vladancupric@gmail.com>
2023-01-08 08:58:12 +0100

    Add

--- task14/main.js
+++ task14/main.js
@@ -5,7 +5,7 @@ for (const elem of elementi_galerije) {
   // Zamenjujemo sve `span` elemente `img` elementima
   if (elem.tagName.toLowerCase() === 'span') {
     const slika = document.createElement('img');
-    slika.src = 'portret.jpg';
+    slika.src = 'https://static.mondo.rs/Pictures/950/1689/1137888/jpeg/josip-broz.jpg?ts=2022-04-24T22:22:41';
     slika.width = 100;
 
     galerija.replaceChild(/* šta ubacujemo */ slika, /* šta izbacujemo */ elem);
