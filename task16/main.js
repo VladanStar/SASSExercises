85977b269d99dd25aae7d4e632b2c5f910109982
VladanStar <vladancupric@gmail.com>
2023-04-03 14:46:55 +0200

    Add

--- task16/main.js
+++ task16/main.js
@@ -1,23 +1,23 @@
-const jokeEl = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
 
-jokeBtn.addEventListener('click', generateJoke)
+jokeBtn.addEventListener("click", generateJoke);
 
-generateJoke()
+generateJoke();
 
 // USING ASYNC/AWAIT
 async function generateJoke() {
   const config = {
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
-  }
+  };
 
-  const res = await fetch('https://icanhazdadjoke.com', config)
+  const res = await fetch("https://icanhazdadjoke.com", config);
 
-  const data = await res.json()
+  const data = await res.json();
 
-  jokeEl.innerHTML = data.joke
+  jokeEl.innerHTML = data.joke;
 }
 
 // USING .then()
